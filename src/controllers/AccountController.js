04d67950d786c14405eea7f1c230b81c2b5b11de
fardinhakimi@gmail.com <fardin.hakimi@marton-international.com>
2019-08-26 11:29:25 +0200

    minor change

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -2,6 +2,13 @@ const router = require('express').Router()
 const httpStatus = require('http-status-codes')
 const passport = require('passport')
 
+/**
+ * Use passport-jwt strategy to authenticate incoming requests hitting this resource
+ * 
+ * @param req
+ * @param res
+ * @return json
+ */
 router.get('/user', passport.authenticate('jwt', { session: false }),(req, res)=> {
 
     try {
